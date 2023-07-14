9788273c05af4c1233dd5e7921797bfa2302fa06
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-14 23:40:28 +0000

    task 0

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- We are all unique!
+
+
+CREATE TABLE IF NOT EXISTS users(
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        name VARCHAR(255) NOT NULL
+);
