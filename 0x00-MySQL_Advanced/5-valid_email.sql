3a4d40a3738b42d24407526d24b882424f420be6
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-16 01:14:22 +0000

    task 5

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,14 @@
+-- 5-valid_email.sql
+
+DELIMITER $$
+CREATE TRIGGER resetEmail
+BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+        IF OLD.email != NEW.email THEN
+            SET valid_email = 0;
+            -- WHERE email = NEW.email;
+        END IF;
+END;
+$$
+DELIMITER ;
