89f2df9912ad2c1bd46976c790a776ccef55c095
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-20 01:22:07 +0000

    task 0

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-""" Stores the cache class """
+""" Define a class 'cache' """
 import redis
+from typing import Union, Callable
 import uuid
-from typing import Union
 
 
 class Cache:
-    """ Cache Class """
+    """ create a class cache """
     def __init__(self):
+        """ initialize the cache instace """
         self._redis = redis.Redis()
-        self._redis.flushdb()
+        self._redis.flushdb(True)
 
-    def store(self: 'Cache', data: Union[str, bytes, int, float]) -> str:
-        """ Generates and returns a random key """
-        key = str(uuid.uuid1())
-        self._redis.set(key, data)
-        return key
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ A menthod that generate a random key using uuid
+            stores the input data in redis
+            using a random key and returns the key
+        """
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
 
     def get(self, key: str, 
             fn: callable = None
