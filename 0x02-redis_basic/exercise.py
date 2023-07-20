0d097ea45fce0fb431f026aa35414756c8fbb61f
Mamuro oyiboluawhore <missmarvelous2016@gmail.com>
2023-07-20 01:13:31 +0000

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,3 +16,19 @@ class Cache:
         key = str(uuid.uuid1())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, 
+            fn: callable = None
+            ):
+        """ retrives values from redis """
+        data = self._redis.get(key)
+        if data is not None:
+            return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> str:
+        """ get data and converts to string """
+        return self.get(key, fn=lambda x: x.decode('utf-8'))
+
+    def get_int(self, key: str) -> int:
+        """ get data and converts to integer """
+        return self.get(key, fn = lambda x: int(x))
